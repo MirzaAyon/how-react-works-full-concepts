f2fae05cc30051d9913b3c08c348be46c9e77fd4
MirzaAyon <ayonmirza98@gmail.com>
2022-08-05 22:07:56 +0600

    jsx react component babel added

---
+++ jsx-react-component-babel.js
@@ -0,0 +1,27 @@
+// What is JSX, React Component, babel transpiler
+//app.js e jabo
+//google JSX indepth - react.org te jabo
+//pura ta porbo
+//jsx provides syntactical sugar - mane syntex take chomotkar bhabe dekhano
+//jsx provides syntactics sugar for for the react create element
+//boro jinish ke simple hishebe dekhanor kaj ei syntactical sugar er
+//app.js e jei html ache eta html na html er moto dekhte eke bole syntactical suger
+//react wala ra create element diye boro hater diye set kore rakhse tai chooto hater dile kaj hbe na
+//tag er khetre and function er name er khetre both same
+//karon react wala ra build in boro hater diye set kore rakhse
+//jsx easily mistake dhorte pare but babel pare na
+//puro ta porbo article ta important
+
+//JSX er maddhome HTML code er modhe js er expression gula use kora jae {} er maddhome
+//abr jsx er maddhome html er code gula js er modhe  easily likha jae
+//markup language and logic ke alada file e na rekhe react er jnno proyojon e eki file e rakha jae, er jnno react component use kora jae
+//echara jsx e map use korte pari
+//plane html e sudhu atr likhte parbo,js er code likhte parina
+//sudhu nirdhishto part tukue edit kora jae jsx er maddhome r ei jonno key use hy
+//jsx ekta syntactical suger
+
+
+
+//JSX in depth website e niche "the online babel compiler" ache
+//okhane ek side e react er functin dile onno side e babel e dekhabe
+//ekta func , func er vitore return and ekta div diye otar vitore 2 ta tag diye try korbo dekha jabe babel e kivabe change hy
