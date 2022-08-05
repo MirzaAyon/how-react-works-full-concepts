36483d645ede4d9dea53efbd5a9d381e5e1769a3
MirzaAyon <ayonmirza98@gmail.com>
2022-08-05 17:48:53 +0600

    create react app and npx edited

---
+++ 1-create-react-app-under-the-hood.js
@@ -0,0 +1,58 @@
+// for node
+//in cmd
+node--version
+and
+node - v
+
+2 ta same
+
+//node install er sathe sathe npm auto install hye gesilo
+
+npm - v
+ //eta dile npm er version bole dibe
+ //etar meaning node package manager
+ //code chalate gele onk jinish er dorkar pore egula ke manage korar dayitto npm er 
+ //onk gula package er shomonnoy holo npm
+ //ei package er modhe ache library
+
+ //npx ki? google
+ //node package execute-- ja npm er kaj ta easy kore dae 
+
+
+ //create react app? ei jinish ta ki?
+ //etake cli bole mane command line interface
+ //google ke onk shomoy gui bole mane graphical user interface
+
+ //r cmd er part ke bole cli mane command line interface
+
+
+
+
+
+
+ ///ebar react app baniye ta vs e open krbo
+ //package.json e jabo, upore name er mdohe thakbe how react works
+
+
+ //git ignoere e ki ki pathabao ki ki pathabo na ta ache
+
+ //create react app holo react app create korar ekta simple commaand line interface
+ //full  website er html,css,dom ke set korse ei create react app
+ //full script,html,css,dom ke set kore ready korse ei create react app
+
+ //read me te project er bistarito dewa ache
+
+
+ //index.js holo kono ekta application er entry point
+ //documnet.getelementbyid('root') ekhane ache
+ //root element ta ache public er modhe index.html e id="root" dewa ache
+
+ //public-index-no script er kaj holo keo js ke disabled kore felle take noscript theke warning dibe
+
+
+ //readme file e giye control+shift+p dile search bar ashbe 
+ //okhane markdown preview diye search korle ja ashbe ta click korle markdown :open preview dekhabe
+ //readme theke # ba ## e edit kore ekhane dekhte pabo
+
+
+ //markdown cheatsheet diye google krbo readme file edit er shob idea pabo
