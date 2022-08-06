a57254da73307136ca5ddbea43fe888c8a0a0d91
MirzaAyon <ayonmirza98@gmail.com>
2022-08-06 13:29:46 +0600

    dif algo added

---
+++ 47-7.js
@@ -0,0 +1,15 @@
+//virtual dom ki?
+react er virtually build kore dom ache, amdr website e interaction er jnno kothao dom use kora lagle shei dom ta react er virtual dom er sathe compare kore dekhe and shei onujai change kore
+
+
+//google - react diff algorithm
+//kichu pic ache shegula dekhbo
+//virtuad dom ta ora diff algo er maddhome change kore
+
+
+// react fiber architecture - google
+//eta ber kore dekhbo
+//react version 18 e kono kajer performance lack korle er priority komae dite pari, mane late e krbe
+//onk gula loop ba map korle key dite bole ta nahole warning dibe ete change korte shubidha hbe
+//what is react virtual dom? google
+//
