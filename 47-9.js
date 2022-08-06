0df4776fe2ff0f1fd19a197e8360797b172816cc
MirzaAyon <ayonmirza98@gmail.com>
2022-08-06 17:38:19 +0600

    some concepts added

---
+++ 47-9.js
@@ -0,0 +1,7 @@
+// 47-9 Module Summary, recap, Properties vs attributes
+//normally react one way data binding
+//ba uni direction
+
+
+//react component life cycle korbo google
+//eta janbo interview te imp
---
+++ quiz.js
@@ -0,0 +1,5 @@
+//vartual dom is used for increasing performance
+
+
+// সেখানে আমরা দেখবো state হচ্ছে immutable অর্থাৎ চাইলেই সরাসরি চেইঞ্জ করতে পারবে না। আবার জিনিসটা asynchronous ও বটে।
+
